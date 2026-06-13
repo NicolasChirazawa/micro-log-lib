2f5f764f68a0a3a394fd1df006faf4ea24ab87ed
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2026-06-13 01:17:27 -0300

    test: Atualização do playground de teste

--- playground/index.js
+++ playground/index.js
@@ -6,12 +6,40 @@ class ServicoTeste {
     static #name = "ServicoTeste";
 
     static testar() {
-        const options = { colorize: true, 'teste': 'legal', outputMode: 'LOG', type: { 'FATAL': 'RED' } };
-        const teste_2 = new LoggerService(options);
+        const options = { 
+            colorize: true, 
+            'teste': 'legal', 
+            outputMode: 'JSON', 
+            type: { 
+                'FATAL': 'RED' 
+            } 
+        };
 
-        teste_2.fatal("teste", {nome: "falar"}, ServicoTeste);
+        const logger = new LoggerService({options});
+        const logMeta = {
+            data: {
+                'valor': 2,
+            },
+            service_name: ServicoTeste.#name
+        };        
+        const logger_child = logger.child(logMeta);
+
+        const logMeta_2 = {
+            data: {
+                'token': 3,
+            },
+            service_name: ServicoTeste.#name
+        };
+        const logger_child_child = logger_child.child(logMeta_2);
+        
+        console.log(logger_child_child)
+
+        let valor = logger_child_child.fatal(
+            'Teste', 
+            { password: 4 } 
+        );
+        console.log(valor);
     }
 };
-
 
 ServicoTeste.testar();
